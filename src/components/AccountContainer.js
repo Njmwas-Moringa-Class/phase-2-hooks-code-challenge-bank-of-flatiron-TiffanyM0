eede6adf58a76ff8a33f897ffc57fd648ea976bc
TIFFANY MAINA <tiffmaina@gmail.com>
2023-12-14 00:51:49 +0300

    fix error state

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transaction, setTransaction] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+
   return (
     <div>
       <Search />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transaction={transaction} />
     </div>
   );
 }
